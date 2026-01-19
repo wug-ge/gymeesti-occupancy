cafa77159ce777a61b67f38131f38757bdd0a35f
Daniel Wuggenig <github@wug.ge>
2026-01-19 17:07:16 +0100

    edit job alert message

--- apps/web/app/components/JobAlert.vue
+++ apps/web/app/components/JobAlert.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-[#059669] py-1 text-center">
     <!-- <span class="line-through">I'm currently looking for a job</span> -->
-    I found a job!
-    <a class="font-bold underline" href="https://www.linkedin.com/in/daniel-wuggenig-9a1097123/" target="_blank">See where I work!</a>
+    I'm bored, 
+    <a class="font-bold underline" href="https://www.linkedin.com/in/daniel-wuggenig-9a1097123/" target="_blank">chat with me</a>
   </div>
 </template>
